8d41db0de64e6d69ae7c0829334df6cff852d0bb
AbdElrahim Mostafa <abdalrhim122mostafa@gmail.com>
2019-10-31 17:17:50 +0200

    fix an error

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const chat = require('./controller/socket.io.controller').default;
+const chat = require('./controller/socket.io.controller');
 const app = express();
 
 // database connection
