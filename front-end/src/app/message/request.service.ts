a0e250cc3e6a9e9e3c70261f03c60ae894195d3a
AbdElrahim Mostafa <abdalrhim122mostafa@gmail.com>
2019-11-02 16:27:15 +0200

    make request service to handel users chat requests

---
+++ front-end/src/app/message/request.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient , HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { JwtService } from '../jwt.service';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RequestService {
+
+  constructor(private http : HttpClient , private jwt : JwtService) { }
+
+  makeNewRequest(_id) : Observable<Object> {
+    let token = this.jwt.getToken();
+    return this.http.post<Object>(`http://localhost:3000/request/new` , {_id} , {
+      headers :new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      })
+    }).pipe(catchError(this.responseError));
+  }
+
+  getAllRequests() : Observable<Object> {
+    let token = this.jwt.getToken();
+    return this.http.get<Object>(`http://localhost:3000/request/all`, {
+      headers :new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      })
+    }).pipe(catchError(this.responseError));
+  }
+
+  haveRequest(_id) : Observable<Object> {
+    let token = this.jwt.getToken();
+    return this.http.get<Object>(`http://localhost:3000/request/find/${_id}` , {
+      headers :new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      })
+    }).pipe(catchError(this.responseError));
+  }
+  deleteRequest(_id) : Observable<Object> {
+    let token = this.jwt.getToken();
+    return this.http.delete<Object>(`http://localhost:3000/request/delete/${_id}` , {
+      headers :new HttpHeaders({
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${token}`
+      })
+    }).pipe(catchError(this.responseError));
+  }
+
+  responseError(error : HttpErrorResponse) {
+    if(error instanceof ErrorEvent){
+      console.error(`back-end error : ${error.error.message}`);
+    } else {
+      console.error(`back-end developer send this error : ${error.message}`);
+    }
+    return throwError(error);
+  }
+
+}
