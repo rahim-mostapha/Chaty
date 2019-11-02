cfb65c335766743091818fc10a11cf7fc42d95ac
AbdElrahim Mostafa <abdalrhim122mostafa@gmail.com>
2019-11-02 16:06:13 +0200

    make user search and redirect to user

--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users : Object[];
+
+  constructor(private user : UserService , private router : Router) { }
 
   ngOnInit() {
+  }
+  
+  getUsers(ele : any){
+    this.user.searchForUser(ele.value).subscribe(
+      res => {
+        if(res['status'] === 'done') this.users = res['data'];
+      }
+    );
+  }
+
+  randomNumber() : string {
+    return Math.random().toString(16).slice(2, 8);
+  }
+
+  openChat(_id : string){
+    this.router.navigate([`/message/chat/${_id}`]);
   }
 
 }
